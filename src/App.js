da80421f23f2afd2ab143979e0765bcad14ffaaa
gauravrajbhar07 <62788378+gauravrajbhar07@users.noreply.github.com>
2023-07-01 13:58:18 +0530

    resume commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="pdf">
+      <embed
+        className="one"
+        style={{ heigth: "100vh", width: "100vw" }}
+        src="Deedy_CV.pdf"
+      />
+    </div>
+  );
+}
+
+export default App;
